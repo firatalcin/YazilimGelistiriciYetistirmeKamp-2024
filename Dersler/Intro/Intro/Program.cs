5071227a25b7b66be3343feaf4b564cceed12f19
Fırat Alçın <firatalcin04@gmail.com>
2024-02-04 23:23:12 +0300

    Ders 1 Sonu

---
+++ Dersler/Intro/Intro/Program.cs
@@ -0,0 +1,45 @@
+﻿using Intro.Business;
+using Intro.Entities;
+
+namespace Intro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Krediler";
+            int term = 12;
+            double amount = 100000.5;
+
+            //variables -> camelCase
+            bool IsAuthenticated = true;
+
+            Console.WriteLine(message);
+
+            //Condition
+            if (IsAuthenticated)
+            {
+                Console.WriteLine("Hoşgeldin Fırat");
+            }
+
+            string[] loans = { "kredi 1", "kredi 2", "kredi 3", "kredi 4", "kredi 5" };
+
+            for (int i = 0; i < loans.Length; i++)
+            {
+                Console.WriteLine(loans[i]);
+            }
+
+            CourseManager courseManager = new CourseManager();
+            Course[] courses = courseManager.GetAll();
+
+            for (int i = 0; i < courses.Length; i++)
+            {
+                Console.WriteLine(courses[i].Name + " - " + courses[i].Price);
+            }
+
+
+
+            
+        }
+    }
+}
