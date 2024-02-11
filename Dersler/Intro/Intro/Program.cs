9f9cabd7d45ee667075341fd6c8750f8d0359226
Fırat Alçın <firatalcin04@gmail.com>
2024-02-11 22:00:25 +0300

    Gün 2 Ders 1

--- Dersler/Intro/Intro/Program.cs
+++ Dersler/Intro/Intro/Program.cs
@@ -38,8 +38,54 @@ namespace Intro
             }
 
 
+            IndividualCustomer customer1 = new IndividualCustomer();
+            customer1.Id = 1;
+            customer1.NationalIdentity = "12345678911";
+            customer1.FirstName = "Aslı";
+            customer1.LastName = "Karayiğit";
+            customer1.CustomerNumber = "123456";
+            IndividualCustomer customer2 = new IndividualCustomer();
+            customer2.Id = 2;
+            customer2.NationalIdentity = "12345678912";
+            customer2.FirstName = "Özgür";
+            customer2.LastName = "Atılgan";
+            customer2.CustomerNumber = "123457";
 
-            
+            CorporateCustomer customer3 = new CorporateCustomer();
+            customer3.Id = 3;
+            customer3.Name = "KodlamaIO";
+            customer3.CustomerNumber = "654778";
+            customer3.TaxNumber = "1234567891";
+
+            CorporateCustomer customer4 = new CorporateCustomer();
+            customer3.Id = 4;
+            customer3.Name = "Abc";
+            customer3.CustomerNumber = "654779";
+            customer3.TaxNumber = "1234567892";
+
+            int number1 = 10;
+            int number2 = 20;
+            number1 = number2;
+            number2 = 50;
+            Console.WriteLine(number1);
+
+            string[] cities1 = { "Ankara", "İstanbul", "İzmir" };
+            string[] cities2 = { "Bursa", "Bolu", "Diyarbakır" };
+
+            cities2 = cities1;
+            cities1[0] = "Adana";
+
+            Console.WriteLine(cities2[0]);
+
+            BaseCustomer[] customers = { customer1, customer2, customer3, customer4 };
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.CustomerNumber);
+            }
+
+
+
         }
     }
 }
