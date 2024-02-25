a978381d2af836a625c86fec89d9916814edfbac
Fırat Alçın <firatalcin04@gmail.com>
2024-02-25 20:42:27 +0300

    Hafta 2 Ödev 3 Devam Ediyor

---
+++ Odevler/W2H3/DataAccess/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Context
+{
+    public class AppDbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+    }
+}
---
+++ Odevler/W2H3/DataAccess/Repositories/IGenericDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Add(T Entity);
+        void Delete(T Entity);
+        void Update(T Entity);
+        List<T> GetList();
+        T GetById(int id);
+    }
+}
