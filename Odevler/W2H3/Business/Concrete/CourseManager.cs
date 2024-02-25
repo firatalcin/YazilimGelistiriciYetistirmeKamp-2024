e54bf4cb0dc7885cddea7ab8d88eda4360fa34d2
Fırat Alçın <firatalcin04@gmail.com>
2024-02-26 00:22:06 +0300

    Hafta 2 Ödev 3 Tamamlandı

---
+++ Odevler/W2H3/Business/Concrete/CourseManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CourseManager : ICourseService
+    {
+        private readonly ICourseDal _courseDal;
+
+        public CourseManager(ICourseDal courseDal)
+        {
+            _courseDal = courseDal;
+        }
+
+        public void Create(Course course)
+        {
+            _courseDal.Add(course);
+        }
+
+        public List<Course> GetAll()
+        {
+            return _courseDal.GetList();
+        }
+    }
+}
--- Odevler/W2H3/DataAccess/Abstract/IInstructorDal.cs
+++ Odevler/W2H3/DataAccess/Abstract/IInstructorDal.cs
@@ -8,8 +8,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface ICategoryDal : IGenericDal<Category>
-    {
-
+    public interface IInstructorDal : IGenericDal<Instructor>
+    { 
     }
 }
