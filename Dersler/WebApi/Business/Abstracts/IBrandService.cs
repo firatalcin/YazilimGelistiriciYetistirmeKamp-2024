eb4a723ec773788e17c2a6fedc7d0108a69c393d
Fırat Alçın <firatalcin04@gmail.com>
2024-02-25 21:34:03 +0300

    Hafta 3 Ders 1

---
+++ Dersler/WebApi/Business/Abstracts/IBrandService.cs
@@ -0,0 +1,12 @@
+﻿using Business.Dtos.Requests;
+using Business.Dtos.Responses;
+using Entities.Concretes;
+
+namespace Business.Abstracts
+{
+    public interface IBrandService
+    {
+        CreatedBradResponse Add(CreateBrandRequest brand);
+        List<Brand> GetAll();
+    }
+}
